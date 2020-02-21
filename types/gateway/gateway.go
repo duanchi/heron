a2eceac8a4232b8685d72d1cfed9f7b949a0c193
duanchi <07.duanchi@gmail.com>
2020-02-21 22:05:20 +0800

    修改GatewayData中More字段为map

--- types/gateway/gateway.go
+++ types/gateway/gateway.go
@@ -4,7 +4,7 @@ type Data struct {
 	Data struct{
 		Token string `json:"token"`
 		User string `json:"user"`
-		More string `json:"more"`
+		More map[string]interface{} `json:"more"`
 	} `json:"data"`
 	Url string `json:"url"`
 }
