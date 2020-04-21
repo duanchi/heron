6a20a0748e091671327c8db2cb44789503baea63
duanchi <07.duanchi@gmail.com>
2020-04-21 10:00:29 +0800

    完成model

--- interface/model.go
+++ interface/model.go
@@ -1,9 +1,5 @@
 package _interface
 
-import "github.com/xormplus/xorm"
-
 type ModelInterface interface {
-	SetEngine (name string)
-	SetDb (engine *xorm.Engine)
 	Options () map[string]interface{}
 }
